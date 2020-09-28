d8e8eb83afc65272b8c88552a38d9784f89db948
tornike <tornike.tsikhelishvili@gmail.com>
2020-09-28 16:03:45 +0400

    some

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:car_rent/main.dart';
+import 'package:flutter_starter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
